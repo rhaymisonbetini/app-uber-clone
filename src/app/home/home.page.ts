2c5c98c5ad4fb3513792620b4c2dfb3ef0e5f38b
RHAYMISON <rhaymison@hotmail.com>
2019-11-24 14:11:32 -0300

    polylines + interface + ajustes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,12 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, NgZone } from '@angular/core';
 import { Platform, LoadingController } from '@ionic/angular';
-import { Environment, GoogleMap, GoogleMaps, GoogleMapOptions, GoogleMapsEvent, GoogleMapsAnimation } from '@ionic-native/google-maps';
+import {
+  Environment, GoogleMap, GoogleMaps, GoogleMapOptions,
+  GoogleMapsEvent, GoogleMapsAnimation, Marker, Geocoder, ILatLng
+} from '@ionic-native/google-maps';
+import { async } from 'q';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -9,11 +15,19 @@ import { Environment, GoogleMap, GoogleMaps, GoogleMapOptions, GoogleMapsEvent, 
 })
 export class HomePage implements OnInit {
 
-  constructor(private platform: Platform, private loadingCtrl: LoadingController) { }
-
   @ViewChild('map', { static: true }) mapElement: any;
   private loading: any;
   private map: GoogleMap;
+  public search = '';
+  private googleAutoComplete = new google.maps.places.AutocompleteService();
+  private googleDirectionService = new google.maps.DirectionsService();
+  public searchResults = new Array<any>();
+  private originMarker: Marker;
+  public destination: any;
+
+  constructor(private platform: Platform, private loadingCtrl: LoadingController, private ngZone: NgZone) {
+    console.log(this.googleAutoComplete);
+  }
 
   ngOnInit() {
     this.mapElement = this.mapElement.nativeElement;
@@ -29,8 +43,8 @@ export class HomePage implements OnInit {
     await this.loading.present();
 
     Environment.setEnv({
-      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyCXYlrKX3x3MRFqf8VLipI-FLz0DmJDRu8',
-      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyCXYlrKX3x3MRFqf8VLipI-FLz0DmJDRu8'
+      API_KEY_FOR_BROWSER_RELEASE: '',
+      API_KEY_FOR_BROWSER_DEBUG: ''
     });
 
     const mapOptions: GoogleMapOptions = {
@@ -56,7 +70,7 @@ export class HomePage implements OnInit {
         zoom: 18
       });
 
-      this.map.addMarkerSync({
+      this.originMarker = this.map.addMarkerSync({
         title: 'Origem',
         icon: '#000',
         animation: GoogleMapsAnimation.DROP,
@@ -70,8 +84,64 @@ export class HomePage implements OnInit {
   }
 
   // search
+  onSearchChange() {
+    if (!this.search.trim().length) { return; }
+    this.googleAutoComplete.getPlacePredictions({ input: this.search }, prediction => {
+      this.ngZone.run(() => {
+        this.searchResults = prediction;
+      });
+    });
+  }
 
-  onSearchChange(event) {
+  async onCalcRoute(result: any) {
+    this.search = '';
+    this.destination = result;
 
+    const info: any = await Geocoder.geocode({
+      address: this.destination.description
+    });
+
+    const markerDestination: Marker = this.map.addMarkerSync({
+      title: this.destination.description,
+      icon: '#5cb85c',
+      animation: GoogleMapsAnimation.BOUNCE,
+      position: info[0].position
+    });
+
+    this.googleDirectionService.route({
+      origin: this.originMarker.getPosition(),
+      destination: markerDestination.getPosition(),
+      travelMode: 'DRIVING'
+    }, async results => {
+
+      const pointers = new Array<ILatLng>();
+      const routes = results.routes[0].overview_path;
+
+      for (let i = 0; i < routes.length; i++) {
+        pointers[i] = {
+          lat: routes[i].lat(),
+          lng: routes[i].lng()
+        };
+      }
+      await this.map.addPolyline({
+        points: pointers,
+        color: '#000',
+        width: 3
+      });
+
+      await this.map.moveCamera({ target: pointers });
+      this.map.panBy(0, 100);
+    });
+  }
+
+
+  async onBack() {
+    try {
+      await this.map.clear();
+      this.destination = null;
+      this.addOrginMarker();
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
